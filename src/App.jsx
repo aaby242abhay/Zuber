4684cec2a731d75dcc7fc06236688eb8ec8678ba
Aaby Abhay <aaby242abhay@gmail.com>
2024-07-24 16:16:00 +0530

    responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div style = {{display : "flex", justifyContent : "space-around"}}>
-        <div style = {{backgroundColor: "red"}}>Hii</div>
-        <div style = {{backgroundColor: "blue"}}>Hii</div>
-        <div style = {{backgroundColor: "pink"}}>Hii</div>
-      </div>
-      <div className='flex justify-center'>
-        <div className='bg-red-500 '>Hii</div>
-        <div className='bg-yellow-500'>Hii</div>
-        <div className='bg-pink-500'>Hii</div>
-      </div>
-
-      <div className='flex '>
-        <div className='bg-red-500 w-[30%]'>Hii</div>
-        <div className='bg-yellow-500 w-[30%]'>Hii</div>
-        <div className='bg-pink-500 w-[30%]'>Hii</div>
-      </div>
-
-      <div className='grid grid-cols-12'>
-        <div className='bg-red-500 col-span-4'>Hii</div>
-        <div className='bg-yellow-500 col-span-4'>Hii</div>
-        <div className='bg-pink-500 col-span-2'>Hii</div>
+      <div className='grid grid-cols-1 md:grid-cols-3'>
+        <div className='bg-blue-500'>1</div>
+        <div className='bg-pink-500'>2</div>
+        <div className='bg-green-500'>3</div>
       </div>
     </>
   )
