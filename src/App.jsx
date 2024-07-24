8b4992ff3f6c0eb15bf4de9e7e3d9993b6751bea
Aaby Abhay <aaby242abhay@gmail.com>
2024-07-24 17:48:48 +0530

    revenueCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
+import {RevenueCard} from './components/RevenueCard'
 
 function App() {
 
   return (
     <>
-      <div className='grid grid-cols-1 md:grid-cols-3'>
-        <div className='bg-blue-500'>1</div>
-        <div className='bg-pink-500'>2</div>
-        <div className='bg-green-500'>3</div>
+      <div className='grid grid-cols-4'>
+      <RevenueCard title={"Amount Pending"} amount = {"92,312.20"} orderCount = {13}/>
       </div>
     </>
   )
